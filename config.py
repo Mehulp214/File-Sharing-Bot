0633c6c41a61eb9c9bae402bc03cbfbfc1a7f5f5
Mehulp214 <114227971+Mehulp214@users.noreply.github.com>
2024-05-22 12:08:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,8 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://QWERTY_18383:Qwerty_1234@
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001740779320"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+#FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001740779320"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
