4ad089bede2716268391ec285e6a703cdf6fd7ff
Mehulp214 <114227971+Mehulp214@users.noreply.github.com>
2024-07-08 19:17:10 +0530

    Update foce_sub.py

--- plugins/foce_sub.py
+++ plugins/foce_sub.py
@@ -82,11 +82,11 @@
 import logging
 
 from pyrogram import filters
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from bot import Bot
-from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT
-from helper_func import subscribed, encode, decode, get_messages
-from database.database import add_user, del_user, full_userbase, present_user, add_fsub_channel, remove_fsub_channel, get_fsub_channels, enable_fsub, disable_fsub, is_fsub_enabled
+from config import ADMINS
+from database.database import add_fsub_channel, remove_fsub_channel, get_fsub_channels, enable_fsub, disable_fsub
+
 
 @Bot.on_message(filters.command('adminpanel') & filters.private & filters.user(ADMINS))
 async def admin_panel(client: Bot, message: Message):
